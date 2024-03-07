d1e12bff6cee7af53ff9622ee71b435d5d53ca6a
Yönet <3523671+Yonet@users.noreply.github.com>
2024-03-07 21:04:00 +0300

    Add scattered layout function

---
+++ src/app/three/layout.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class LayoutService {
+
+  constructor() { }
+
+  // TODO: Sphere, Cylinder, grid, Plane, scatter, Grid, Prism Layouts
+
+  /**
+   * parent object is anchored to the top/middle/bottom and left/center/right of the collection. 
+   * Layout order: https://learn.microsoft.com/en-us/windows/mixed-reality/mrtk-unity/mrtk2/features/ux-building-blocks/object-collection?view=mrtkunity-2022-05#gridobjectcollection-layout-order
+   */
+  gridLayout () {
+
+  }
+  /**
+   * Takes number of objects and their width
+   * Returns a sphere Layout
+   * */
+  sphereLayout ( ops: any ) {
+
+  }
+  /**
+   * 
+   * @param ops Takes an array of objects and a range
+   * returs scattered objects 
+   */
+  scatterLayout ( ops: any ) {
+    let w = ops.width || window.innerWidth;
+    let h = ops.width || window.innerHeight;
+    let d = ops.depth || 200;
+    for ( let i = 0; i < ops.objects.length; i++ ) {
+      ops.objects[i].position.x = w * Math.random() - w / 2;
+      ops.objects[i].position.y = h * Math.random() - h / 2;
+      ops.objects[i].position.z = d * Math.random() - d;
+    }
+  }
+
+}
