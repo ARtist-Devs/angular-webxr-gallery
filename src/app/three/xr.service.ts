92d052a9f173d7e3a538b78e828a664599e54b0c
Yönet <3523671+Yonet@users.noreply.github.com>
2024-05-06 19:15:06 +0300

    XR clean up

--- src/app/three/xr.service.ts
+++ src/app/three/xr.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 
 import * as m from 'gl-matrix';
 import { AdditiveBlending, BufferGeometry, Float32BufferAttribute, Line, LineBasicMaterial, Mesh, MeshBasicMaterial, Ray, RingGeometry, Vector3, WebGLRenderer } from 'three';
 import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
+import { InteractionsService } from './interactions.service';
 
 
 @Injectable( {
   providedIn: 'root'
 } )
 export class XRService {
+  private interactions = inject( InteractionsService );
+
   vrSupported = false;
   arSupported = false;
   xrMode: WritableSignal<string> = signal( 'inline' );
@@ -33,29 +36,31 @@ export class XRService {
   checkXRSupport ( ops: { renderer: WebGLRenderer, camera: any, scene: any; } ) {
     if ( this.vrSupported || this.arSupported ) { return true; }
     else if ( navigator.xr ) {
-      // Starts the inline session and init AR/VR depending on xrMode
 
+      // Starts the inline session and init AR/VR depending on xrMode
       navigator.xr.isSessionSupported( 'immersive-vr' ).then( ( supported ) => {
         this.vrSupported = true;
         this.xrMode.set( 'immersive-vr' );
-        console.log( 'VR supported ', supported );
       } );
 
       navigator.xr.isSessionSupported( 'immersive-ar' ).then( ( supported ) => {
         this.arSupported = true;
-        console.log( 'AR supported ', supported );
         this.xrMode.set( 'immersive-ar' );
       } );
 
-
       this.initXR( ops );
+
       return true;
+
     }
+
     return false;
+
   }
 
   // Initiate an XR session 
   initXR ( ops: any ) {
+
     this.renderer = ops.renderer;
     this.scene = ops.scene;
     this.webXRManager = ops.renderer.xr;
@@ -67,6 +72,7 @@ export class XRService {
     } else if ( this.xrMode() === 'immersive-ar' ) {
       this.initAR();
     }
+
   }
 
   initVR () {
@@ -110,11 +116,6 @@ export class XRService {
 
     } );
 
-    controller.addEventListener( 'disconnected', function () {
-      console.log( 'Controller disconnected' );
-
-    } );
-
     const controllerModelFactory = new XRControllerModelFactory();
 
     const controllerGrip = this.renderer.xr.getControllerGrip( i );
@@ -141,31 +142,29 @@ export class XRService {
   onSelectStart ( e: any ) {
     console.log( 'Select Start VR event ', e );
     this.userData.isSelecting = true;
-    // this.interactions.intersectObjects({ controller: this.controllerLeft, scene: this.scene });
-    // this.interactions.intersectObjects({ controller: this.controllerRight, scene: this.scene });
+    this.interactions.intersectObjects( { controller: this.controllerLeft, scene: this.scene } );
+    this.interactions.intersectObjects( { controller: this.controllerRight, scene: this.scene } );
 
   };
 
   onSelect ( e: Event ) {
     console.log( 'Select VR event ', e );
-    // this.interactions.intersectObjects( { controller: this.controllerLeft, scene: this.scene, select: true } );
-    // this.interactions.intersectObjects( { controller: this.controllerRight, scene: this.scene, select: true } );
+    this.interactions.intersectObjects( { controller: this.controllerLeft, scene: this.scene, select: true } );
+    this.interactions.intersectObjects( { controller: this.controllerRight, scene: this.scene, select: true } );
   }
 
   onSelectEnd ( e: any ) {
     console.log( 'Select End VR event ', e );
     this.userData.isSelecting = false;
-    // TODO: Needed for hover events only
-    // this.interactions.intersectObjects({ controller: this.controllerLeft, scene: this.scene });
+    this.interactions.intersectObjects( { controller: this.controllerLeft, scene: this.scene } );
   }
 
   initAR () {
     console.log( 'Initializing AR' );
-
   }
 
   updateInputSources ( session = this.session, frame: any, refSpace: any, scene = this.scene ) {
-    console.log( 'updateInputSources ', session, frame, refSpace, scene );
+
     let vec3 = m.vec3;
     for ( let inputSource of session.inputSources ) {
       let targetRayPose = frame.getPose( inputSource.targetRaySpace, refSpace );
@@ -202,7 +201,6 @@ export class XRService {
         targetRay.direction.y * cursorDistance,
         targetRay.direction.z * cursorDistance,
       ] );
-      // vec3.transformMat4(cursorPos, cursorPos, inputPose.targetRay.transformMatrix);
 
       scene.inputRenderer.addCursor( cursorPos );
 
@@ -219,21 +217,4 @@ export class XRService {
 
   }
 
-  // onXRFrame(t: any, frame: any) {
-  //   let session = frame.session;
-  //   let refSpace = session.isImmersive ?
-  //     xrImmersiveRefSpace :
-  //     this.inlineViewerHelper.referenceSpace;
-  //   let pose = frame.getViewerPose(refSpace);
-
-  //   this.scene.startFrame();
-
-  //   session.requestAnimationFrame(this.onXRFrame);
-
-  //   this.updateInputSources(session, frame, refSpace);
-
-  //   this.scene.drawXRFrame(frame, pose);
-
-  //   this.scene.endFrame();
-  // }
 }
