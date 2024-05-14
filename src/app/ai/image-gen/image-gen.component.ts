b392b7e0ceae3fe35595da45973ed27484ef5606
Minko Gechev <mgechev@gmail.com>
2024-05-14 11:47:26 -0700

    Prepare for a release (#27)
    * Prepare for release
    
    * Add license headers
    
    * Add error handling and use npm
    
    * Add error handling to the backend and the frontend
    
    Also update the URL to use localhost instead of
    the address assigned from the local network

--- src/app/ai/image-gen/image-gen.component.ts
+++ src/app/ai/image-gen/image-gen.component.ts
@@ -1,9 +1,27 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { NgClass } from '@angular/common';
 import { Component, inject, output, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Artwork } from '../../artworks.service';
 import { GenerativeService } from '../generative.service';
+import { catchError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component( {
   selector: 'art-image-gen',
@@ -15,10 +33,12 @@ import { GenerativeService } from '../generative.service';
 export class ImageGenComponent {
   private generative = inject( GenerativeService );
 
-  newArtworksEvent = output<Artwork[]>();
-  prompt: string = '';
-  question: string = '';
-  message: WritableSignal<string> = signal( 'Welcome to WebXR Generative AI Art Gallery!' );
+  protected newArtworksEvent = output<Artwork[]>();
+  protected prompt = '';
+  protected question = '';
+  protected message = signal( 'Welcome to WebXR Generative AI Art Gallery!' );
+  protected requestPending = false;
+  protected error = '';
 
   genImages () {
 
@@ -26,25 +46,36 @@ export class ImageGenComponent {
     this.prompt = this.prompt == '' ? 'A steampunk era science lab with a stylish figure in silhouette with dramatic lighting and vibrant colors dominated with copper hue' : this.prompt;
     this.question = this.question == '' ? 'Describe the image and tell me what makes this artwork beautiful' : this.question;
 
+    this.requestPending = true;
+    this.error = null;
+
     // Call the service to generate image and emit the new image info
-    this.generative.generateImages( { prompt: this.prompt, question: this.question } ).subscribe( ( response ) => {
+    this.generative.generateImages( { prompt: this.prompt, question: this.question } )
+    .subscribe( {
+      next: response => {
 
-      let images: Artwork[] = [];
-      // @ts-expect-error
-      response.map( ( data, i ) => {
-        const image: Artwork = {
-          id: i,
-          url: `data:image/png;base64,${data.image}`,
-          description: `${data.caption}`,
-          title: data.title
-        };
-        images.push( image );
-
-      } );
-
-      this.newArtworksEvent.emit( images );
-
-    } );
+        this.requestPending = false;
+  
+        let images: Artwork[] = [];
+        // @ts-expect-error
+        response.map( ( data, i ) => {
+          const image: Artwork = {
+            id: i,
+            url: `data:image/png;base64,${data.image}`,
+            description: `${data.caption}`,
+            title: data.title
+          };
+          images.push( image );
+  
+        } );
+  
+        this.newArtworksEvent.emit( images );
+  
+      }, error: error =>{
+        this.error = JSON.stringify(error.error);
+        this.requestPending = false;
+      }
+    });
 
   }
 
